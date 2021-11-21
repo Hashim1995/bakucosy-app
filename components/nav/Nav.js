02573b144300f2019de282d2633915e72905a548
Hashim Hashimli <hashimli.hshm@gmail.com>
2021-11-21 15:55:03 +0400

    first commit

---
+++ components/nav/Nav.js
@@ -0,0 +1,82 @@
+import React from "react";
+import Link from "next/link";
+import { Row, Col } from "antd";
+import Style from "./Nav.module.scss";
+import Image from "next/image";
+import logo from "../../assets/images/logo-black.png";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import CategoryOutlinedIcon from "@mui/icons-material/CategoryOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import PhoneEnabledOutlinedIcon from "@mui/icons-material/PhoneEnabledOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
+import { Menu } from "antd";
+const Nav = () => {
+  const { SubMenu } = Menu;
+  return (
+    <>
+      <nav>
+        <Row></Row>
+      </nav>
+      <Menu className={Style.menu} mode="horizontal">
+        <div className={Style.menuLeftGroup}>
+          <Menu.Item className={Style.menuItem} icon={<HomeOutlinedIcon />}>
+            <Link href="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item
+            className={Style.menuItem}
+            icon={<ShoppingCartOutlinedIcon />}
+          >
+            <Link href="/Products">Products </Link>
+          </Menu.Item>
+          <Menu.Item className={Style.menuItem} icon={<CategoryOutlinedIcon />}>
+            <Link href="/Categories">Categories</Link>
+          </Menu.Item>
+          <Menu.Item
+            className={Style.menuItem}
+            icon={<HelpOutlineOutlinedIcon />}
+          >
+            <Link href="/About">About</Link>
+          </Menu.Item>
+          <Menu.Item
+            className={Style.menuItem}
+            icon={<PhoneEnabledOutlinedIcon />}
+          >
+            <Link href="/Contact">Contact</Link>
+          </Menu.Item>
+        </div>
+        <div className={Style.menuCenterGroup}>
+          <Menu.Item>
+            <Link passHref={true} href="/">
+              <a>
+                {" "}
+                <Image
+                  className={Style.navLogo}
+                  alt="baku cosy shop logo"
+                  src={logo}
+                />
+              </a>
+            </Link>
+          </Menu.Item>
+        </div>
+        <div className={Style.menuRightGroup}>
+          <Menu.Item
+            className={Style.menuItem}
+            icon={<AccountCircleOutlinedIcon />}
+          >
+            <Link href="/login">sign in</Link>
+          </Menu.Item>
+          <Menu.Item
+            className={Style.menuItem}
+            icon={<AddShoppingCartOutlinedIcon />}
+          >
+            <Link href="/Cart">Cart</Link>
+          </Menu.Item>
+        </div>
+      </Menu>
+    </>
+  );
+};
+
+export default Nav;
