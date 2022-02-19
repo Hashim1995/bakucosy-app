4beb3a7c989a8eca575858b1ec1a9a90dd5b04a6
Hashim Hashimli <hashimli.hshm@gmail.com>
2022-02-19 13:36:49 +0400

    font error fix test-2

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,19 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Shizuru&display=swap"
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
+          crossOrigin="anonymous"
         />
       </Head>
       <body>
