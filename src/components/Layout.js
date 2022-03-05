4f044ddc07f60f03554cade788ddb68143aecd34
Hashim Hashimli <hashimli.hshm@gmail.com>
2022-03-06 00:10:42 +0400

    redux wrap whole page, Done

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,18 @@
 import React from "react";
 import Nav from "./nav/Nav";
 import Footer from "./Footer/Footer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { reactLocalStorage } from "reactjs-localstorage";
+
+import { setCurrentUser } from "../../redux/currentUser";
 const Layout = ({ children }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const loggedUser = reactLocalStorage.getObject("loggedUser");
+
+    dispatch(setCurrentUser(loggedUser));
+  });
   return (
     <div>
       <Nav />
