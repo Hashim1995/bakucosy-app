76c7ff9df980d80648b6ff020097aebcb65ccd7e
Hashim Hashimli <hashimli.hshm@gmail.com>
2022-04-21 18:35:15 +0400

    updated cors config v-8

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
   images: {
     domains: ["i.ibb.co"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/product/:path*",
+        destination: "https://bakucosybackend.vercel.app/productlist/:path*",
+      },
+    ];
+  },
 };
